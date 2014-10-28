978147f73612c931521302de857a0a8832aaddc7
undrepopov <andrepopov@gmail.com>
2014-10-28 01:05:32 -0500

    added a test class

---
+++ Git Test/ATest.h
@@ -0,0 +1,13 @@
+//
+//  ATest.h
+//  Git Test
+//
+//  Created by User on 10/28/14.
+//  Copyright (c) 2014 ANDREY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATest : NSObject
+
+@end
---
+++ Git Test/ATest.m
@@ -0,0 +1,13 @@
+//
+//  ATest.m
+//  Git Test
+//
+//  Created by User on 10/28/14.
+//  Copyright (c) 2014 ANDREY. All rights reserved.
+//
+
+#import "ATest.h"
+
+@implementation ATest
+
+@end
